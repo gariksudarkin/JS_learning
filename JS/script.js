5bea0da7f0bca163a2629409ad8db13e38a888df
profi_110 <heorhiisudarkin@gmail.com>
2023-01-10 17:27:53 +0200

    changes 1

--- JS/script.js
+++ JS/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
 };
 
 
-const a = prompt("last seen film", ''),
-      b = prompt('score', ''),
-      c = prompt("last seen film", ''),
-      d = prompt('score', '');
+// const a = prompt("last seen film", ''),
+//       b = prompt('score', ''),
+//       c = prompt("last seen film", ''),
+//       d = prompt('score', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("last seen film", ''),
+        b = prompt('score', '');
+
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('пользователь все ввел правильно');
+    } else {
+        i--;
+        console.log 
+    }
+       
+}
 
 console.log(personalMovieDB);
